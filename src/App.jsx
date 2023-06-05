2f743140ccc4fb48d6b4f611836bf9e87cf21743
Evan <evan.lueber@stud.kbw.ch>
2023-06-05 13:11:38 +0200

    dolfer structure done and router installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './About/page';
+import Contact from './Contact/page'
+import Imprint from './Imprint/page'
+import Offers from './Offers/page'
+
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route to="" Component={About} />
+          <Route to="" Component={Contact} />
+          <Route to="" Component={Imprint} />
+          <Route to="" Component={Offers} />
+        </Routes>
+      </BrowserRouter>
       <header className="App-header">
         
       </header>
---
+++ src/Imprint/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Imprint = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Imprint;
