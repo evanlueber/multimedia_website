49e19b0d322684f51d9dfe2a4176b2db11418344
Evan <evan.lueber@stud.kbw.ch>
2023-06-07 14:07:33 +0200

    homePage titlecontainer designed

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -3,31 +3,33 @@ import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div className="navigation">
-      <div className="navItem">
-          <Link to="/">
-                <p className="navItemText">Home</p>
-          </Link>
-      </div>
-      <div className="navItem">
-          <Link to="/Offers">
-                <p className="navItemText">Angebote</p>
-          </Link>
-      </div>
-      <div className="navItem">
-          <Link to="/About">
-                <p className="navItemText">Über mich</p>
-          </Link>
-      </div>
-      <div className="navItem">
-          <Link to="/Contact">
-                <p className="navItemText">Kontakt</p>
-          </Link>
-      </div>
-      <div className="navItem">
-          <Link to="/Imprint">
-                <p className="navItemText">Impressum</p>
-          </Link>
+    <div className="navigationBlur">
+      <div className="navigationBar">
+        <div className="navItem">
+            <Link to="/">
+                  <p className="navItemText">Home</p>
+            </Link>
+        </div>
+        <div className="navItem">
+            <Link to="/Offers">
+                  <p className="navItemText">Angebote</p>
+            </Link>
+        </div>
+        <div className="navItem">
+            <Link to="/About">
+                  <p className="navItemText">Über mich</p>
+            </Link>
+        </div>
+        <div className="navItem">
+            <Link to="/Contact">
+                  <p className="navItemText">Kontakt</p>
+            </Link>
+        </div>
+        <div className="navItem">
+            <Link to="/Imprint">
+                  <p className="navItemText">Impressum</p>
+            </Link>
+        </div>
       </div>
     </div>
   );
--- src/Home/page.jsx
+++ src/Home/page.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="homePage">
+      <div className="homeContainerBlur">
+          <div className="titleContainer">
+            <h1 className="title">
+              Willkommen in dem Botanischen Garten der Universität Zürich
+            </h1>
+          </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
