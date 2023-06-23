b6d78c6e43449c40fd598e13cc3b7899f42aa07c
Evan <evan.lueber@stud.kbw.ch>
2023-06-23 14:27:56 +0200

    sliders done

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,27 +1,20 @@
 import React from "react";
-import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai";
+import {
+  ReactCompareSlider,
+  ReactCompareSliderImage,
+} from "react-compare-slider";
 
-const Slider = ({ img1, img2, width, height, sliderHeight }) => {
+const Slider = ({ size, portrait, img1, img2 }) => {
   return (
-    <div className="slider">
-      <div>
-        <img className="sliderImg" src={img1} alt="" width={width} height={height} />
-        <div>
-          <div className="sliderIcons">
-            <AiFillCaretLeft />
-            <AiFillCaretRight />
-          </div>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 0.5 10" height={sliderHeight}>
-            <path
-              d="M 0 0 V 10 Z"
-              stroke="#FFFFFF"
-              stroke-width="1"
-              fill="none"
-            />
-          </svg>
-        </div>
-        <img className="sliderImg" src={img2} alt="" width={width} height={height} />
-      </div>
+    <div style={size}>
+      <ReactCompareSlider portrait={portrait}
+        itemOne={
+          <ReactCompareSliderImage src={img1}/>
+        }
+        itemTwo={
+          <ReactCompareSliderImage src={img2}/>
+        }
+      />
     </div>
   );
 };
--- src/Home/page.jsx
+++ src/Home/page.jsx
@@ -1,62 +1,118 @@
 import React from "react";
 import Navigation from "../Components/Navigation";
 import ImageMagnifier from "../Components/ImageMagnifier";
-import Slider from "../Components/Slider";
+import ImageCompareSlider from "../Components/Slider";
 
 const Home = () => {
   return (
-    <div className="homePage">
-      <Navigation />
-      <div className="pictureIdeas">
-        <div className="titleWrapper">
-          <div className="homeContainerBlur">
-            <div className="titleContainer">
-              <h1>
-                Willkommen auf der Multimedia Website des Botanischen Gartens
-                der Universität Zürich
-              </h1>
+    <div className="page">
+      <div className="homePage">
+        <Navigation />
+        <div className="pictureIdeas">
+          <div className="titleWrapper">
+            <div className="homeContainerBlur">
+              <div className="titleContainer">
+                <h1>
+                  Willkommen auf der Multimedia Website des Botanischen Gartens
+                  der Universität Zürich
+                </h1>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="pictureContainer">
-          <h2>HORIZONTAL SLIDER</h2>
-          <div className="images">
-            <div className="imageContainer">
-              <Slider
-                img1="/img/weissGruene_blaetter.jpg"
-                img2="/img/weissGruene_blaetter_edited.png"
-                width={""}
-                height={"325px"}
-                sliderHeight={"325px"}
-              />
+          <div className="pictureContainer">
+            <h2>HORIZONTAL SLIDER</h2>
+            <div className="images">
+              <div className="imageContainer">
+                <ImageCompareSlider
+                  img1="/img/weissGruene_blaetter_edited.png"
+                  img2="/img/weissGruene_blaetter.jpg"
+                  size = {{
+                    height: "auto",
+                    width: "450px"
+                  }}
+                  portrait={false}
+                />
+              </div>
+              <div className="imageContainer">
+                <ImageCompareSlider
+                  img1="/img/lange_stachelblaetter_edited.png"
+                  img2="/img/lange_stachelblaetter.jpg"
+                  size = {{
+                    height: "auto",
+                    width: "450px"
+                  }}
+                  portrait={false}
+                />
+              </div>
             </div>
+            <p className="explanation">
+              Ziehen Sie den Slider nach links oder rechts
+            </p>
           </div>
-        </div>
-        <div className="pictureContainer">
-          <h2>VERTICAL SLIDER</h2>
-        </div>
-        <div className="pictureContainer">
-          <h2 className="ideaTitle">IMAGE MAGNIFIER</h2>
-          <div className="images">
+          <div className="pictureContainer">
+            <h2>VERTICAL SLIDER</h2>
+            <div className="images">
+              <div className="imageContainer">
+                <ImageCompareSlider
+                  img1="/img/runder_kaktus.jpg"
+                  img2="/img/runder_kaktus_edited.png"
+                  size = {{
+                    height: "auto",
+                    width: "325px"
+                  }}
+                  portrait={true}
+                />
+              </div>
+              <div className="imageContainer">
+                <ImageCompareSlider
+                  img1="/img/aloevera_edited.png"
+                  img2="/img/aloevera.jpg"
+                  size = {{
+                    height: "auto",
+                    width: "325px"
+                  }}
+                  portrait={true}
+                />
+              </div>
             <div className="imageContainer">
-              <ImageMagnifier width={"325px"} src="/img/weg.jpg" />
-            </div>
-            <div className="imageContainer">
-              <ImageMagnifier width={"325px"} src="/img/glaskuppel.jpg" />
-            </div>
-            <div className="imageContainer">
-              <ImageMagnifier
-                width={"325px"}
-                src="/img/glaskuppe_orangePflanze.jpg"
-              />
-            </div>
+                <ImageCompareSlider
+                  img1="/img/lianenbaum.jpg"
+                  img2="/img/lianenbaum_edited.png"
+                  size = {{
+                    height: "auto",
+                    width: "325px"
+                  }}
+                  portrait={true}
+                />
+              </div>
+              </div>
+            <p className="explanation">
+              Ziehen Sie den Slider nach oben oder unten
+            </p>
           </div>
-          <p className="explanation">
-            Bewegen Sie den Mauszeiger auf eines der Bilder
-          </p>
-        </div>
-        <div className="pictureContainer">
-          <h2>GALLERY</h2>
+          <div className="pictureContainer">
+            <h2 className="ideaTitle">IMAGE MAGNIFIER</h2>
+            <div className="images">
+              <div className="imageContainer">
+                <ImageMagnifier width={"325px"} src="/img/weg.jpg" />
+              </div>
+              <div className="imageContainer">
+                <ImageMagnifier width={"325px"} src="/img/glaskuppel.jpg" />
+              </div>
+              <div className="imageContainer">
+                <ImageMagnifier
+                  width={"325px"}
+                  src="/img/glaskuppe_orangePflanze.jpg"
+                />
+              </div>
+            </div>
+            <p className="explanation">
+              Bewegen Sie den Mauszeiger auf eines der Bilder
+            </p>
+          </div>
+          <div className="pictureContainer">
+            <h2>GALLERY</h2>
+          </div>
         </div>
       </div>
     </div>
