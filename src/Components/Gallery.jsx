c18d8eff3df2b434e94437432884bc25b2b2301a
Evan <evan.lueber@stud.kbw.ch>
2023-06-28 11:00:37 +0200

    gallery component added

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
-const Gallery = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Gallery = ({ images, boxstyle }) => {
+  return (
+    <div>
+      <div style={boxstyle}>
+        <Carousel useKeyboardArrows={true} showThumbs={false} showStatus={false}>
+          {images.map((URL, index) => (
+            <div className="slide">
+              <img alt="sample_file" src={URL} key={index} />
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
