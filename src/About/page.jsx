bb81956d090a55a091a0a84f8e47d99a384d106e
evanlueber <evan.lueber@lernende.bbw.ch>
2023-06-29 23:26:06 +0200

    added offers and styled them

--- src/About/page.jsx
+++ src/About/page.jsx
@@ -5,32 +5,44 @@ const About = () => {
   return (
     <div className="page">
       <Navigation />
-      <div className="titleBlur">
-        <div className="title">
-          <h1>Über uns</h1>
-        </div>
+      <div className="title">
+        <h1>Über uns</h1>
       </div>
       <div className="aboutContentBlur">
         <div className="aboutContentContainer">
           <div className="aboutContent">
-              <h2>Autor</h2>
-              <p>
-                Mein Name ist Evan Lüber und ich bin Schüler an der Kantonschule Büelrain Winterhur, an der ich mache dort die Ausbildung der Informatikmittelschule. Ich bin Programmierer, bin jedoch nicht nur informativ interessiert, sondern mache auch viel Sport.
-              </p>
+            <h2>Autor</h2>
+            <p>
+              Mein Name ist Evan Lüber und ich bin Schüler an der Kantonschule
+              Büelrain Winterhur, an der ich mache dort die Ausbildung der
+              Informatikmittelschule. Ich bin Programmierer, bin jedoch nicht
+              nur informativ interessiert, sondern mache auch viel Sport.
+            </p>
           </div>
           <div className="aboutMidContent">
-              <h2>Botanischer Garten</h2>
-              <p>
-                In einem Botanischen Garten findet man die schönsten und exotischsten Pflanzen, die es auf der Welt gibt. Wenn Sie also eine Pflanze suchen, die Sie noch nie gesehen haben, ist der Botanische Garten der richtige Ort für Sie.
-              </p>
+            <h2>Botanischer Garten</h2>
+            <p>
+              In einem Botanischen Garten findet man die schönsten und
+              exotischsten Pflanzen, die es auf der Welt gibt. Wenn Sie also
+              eine Pflanze suchen, die Sie noch nie gesehen haben, ist der
+              Botanische Garten der richtige Ort für Sie.
+            </p>
           </div>
           <div className="aboutContent">
-              <h2>Ziel dieser Seite</h2>
-              <p>
-                Das Ziel dieser Website ist es, ein paar der wundervollen Pflanzen zu präsentieren und unteranderem meine Programmierfähigkeiten weiter zuentwickeln. Ausserdem konnte ich durch das Bearbeiten der Bilder meine Kompetenz im Thema Bilderbearbeitung steigern. Diese Website beinhaltet Kontent des Botanischen Gartens der Universität Zürich. Die Bilder wurden von mir gemacht und der Text wurde von mir geschrieben.
-              </p>
+            <h2>Ziel dieser Seite</h2>
+            <p>
+              Das Ziel dieser Website ist es, ein paar der wundervollen Pflanzen
+              zu präsentieren und unteranderem meine Programmierfähigkeiten
+              weiter zuentwickeln. Ausserdem konnte ich durch das Bearbeiten der
+              Bilder meine Kompetenz im Thema Bilderbearbeitung steigern. Diese
+              Website beinhaltet Kontent des Botanischen Gartens der Universität
+              Zürich. Die Bilder wurden von mir gemacht und der Text wurde von
+              mir geschrieben.
+            </p>
           </div>
-          <a className="uzhSite" href="https://www.bg.uzh.ch/de.html">Botanischer Garten UZH</a>
+          <a className="uzhSite" href="https://www.bg.uzh.ch/de.html">
+            Botanischer Garten UZH
+          </a>
         </div>
       </div>
     </div>
